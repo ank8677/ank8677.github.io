2e014f6fce1e4feae1dab7d13a76bc87e0a1cc28
ank8677 <87760985+ank8677@users.noreply.github.com>
2021-08-09 19:53:14 +0200

    unfinished version created

---
+++ scripts.js
@@ -0,0 +1,176 @@
+let data0 = {
+    photo: 'images/1.png',
+    title: 'Inside the Forest',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis explicabo illo molestias debitis! Recusandae accusamus, numquam, eos sunt sequi culpa impedit, aperiam voluptatem obcaecati tempore sed distinctio porro ea quaerat.'
+  };
+
+  let data1 = {
+    photo: 'images/2.png',
+    title: 'Lucky Leaves',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis explicabo illo molestias debitis! Recusandae accusamus, numquam, eos sunt sequi culpa impedit, aperiam voluptatem obcaecati tempore sed distinctio porro ea quaerat.'
+  };
+
+  let data2 = {
+    photo: 'images/3.png',
+    title: 'Rainbow',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis explicabo illo molestias debitis! Recusandae accusamus, numquam, eos sunt sequi culpa impedit, aperiam voluptatem obcaecati tempore sed distinctio porro ea quaerat.'
+  };
+
+  let data3 = {
+    photo: 'images/4.png',
+    title: 'Happy Flowers',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis explicabo illo molestias debitis! Recusandae accusamus, numquam, eos sunt sequi culpa impedit, aperiam voluptatem obcaecati tempore sed distinctio porro ea quaerat.'
+  };
+
+  let data4 = {
+    photo: 'images/5.png',
+    title: 'Forest Lake',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis explicabo illo molestias debitis! Recusandae accusamus, numquam, eos sunt sequi culpa impedit, aperiam voluptatem obcaecati tempore sed distinctio porro ea quaerat.'
+  };
+
+  let data5 = {
+    photo: 'images/6.png',
+    title: 'Beauty in Nature',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis explicabo illo molestias debitis! Recusandae accusamus, numquam, eos sunt sequi culpa impedit, aperiam voluptatem obcaecati tempore sed distinctio porro ea quaerat.'
+  };
+
+  let data6 = {
+    photo: 'images/7.png',
+    title: 'Butterflies',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis explicabo illo molestias debitis! Recusandae accusamus, numquam, eos sunt sequi culpa impedit, aperiam voluptatem obcaecati tempore sed distinctio porro ea quaerat.'
+  };
+
+  let data7 = {
+    photo: 'images/8.png',
+    title: 'Towards the Sun...',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis explicabo illo molestias debitis! Recusandae accusamus, numquam, eos sunt sequi culpa impedit, aperiam voluptatem obcaecati tempore sed distinctio porro ea quaerat.'
+  };
+
+  let currentPhoto = 0;
+
+  let imagesData = [data0, data1, data2, data3, data4, data5, data6, data7];
+
+  data0.photoNumber = 0;
+  data1.photoNumber = 1;
+  data2.photoNumber = 2;
+  data3.photoNumber = 3;
+  data4.photoNumber = 4;
+  data5.photoNumber = 5;
+  data6.photoNumber = 6;
+  data7.photoNumber = 7;
+
+  console.log(data7);
+
+ 
+
+  let loadPhoto = (photoNumber) => {
+    $('#photo').attr('src', imagesData[photoNumber].photo);
+    $('#photo-title').text(imagesData[photoNumber].title);
+    $('#photo-description').text(imagesData[photoNumber].description);
+    };
+
+
+  loadPhoto(currentPhoto);
+
+  console.log(imagesData.length);
+
+$('#rightArrow').click(() => {
+   if( currentPhoto < imagesData.length-1){ 
+  currentPhoto++;} 
+  else {
+    currentPhoto = -1;
+    currentPhoto++;
+  };
+  loadPhoto(currentPhoto);
+  console.log(currentPhoto);
+});
+
+
+
+
+console.log(imagesData.length-1);
+
+$('#leftArrow').click(() => {
+    if(currentPhoto > 0) { 
+    currentPhoto--;}
+  else {
+    currentPhoto = 8;
+    currentPhoto--;
+  };
+    loadPhoto(currentPhoto);
+    console.log(currentPhoto);
+  });
+
+let smallPhotos = ['images/1.png', 'images/2.png', 'images/3.png', 'images/4.png', 'images/5.png', 'images/6.png', 'images/7.png', 'images/8.png'];
+console.log(smallPhotos);
+
+smallPhotos.forEach ((smallPhoto, index) => {
+  $('.thumbnails').append(`<div class="thumbnailPhoto visible"><img src = "${smallPhoto}" data-number ="${index}"></div>`);
+  console.log(smallPhoto,index);
+});
+
+let smallPhotosTitles = ['Inside the Forest', 'Lucky Leaves', 'Rainbow', 'Happy Flowers', 'Forest Lake', 'Beauty in Nature', 'Butterflies', 'Towards the Sun...']
+console.log(smallPhotosTitles);
+
+smallPhotosTitles.forEach ((smallPhotoTitle, index) => {
+  $('.title').append(`<div class="thumbnailTitles" data-number="${index}"><p>${smallPhotoTitle} </p></div>`);
+  console.log(smallPhotoTitle,index);
+});
+
+
+$('.thumbnailPhoto').click((event) => {
+  let indexClicked = $(event.target).attr('data-number');
+  let numberIndex = parseInt(indexClicked);
+  console.log(numberIndex);
+  loadPhoto(numberIndex);
+
+  $('#rightArrow').click(() => {
+    if( numberIndex < imagesData.length-1){ 
+  numberIndex++;} 
+  else {
+    numberIndex = -1;
+    numberIndex++;
+  };
+  loadPhoto(numberIndex);
+  console.log(numberIndex);
+  });
+
+  $('#leftArrow').click(() => {
+    if(numberIndex > 0) { 
+    numberIndex--;}
+  else {
+    numberIndex = 8;
+    numberIndex--;
+  };
+    loadPhoto(numberIndex);
+    console.log(numberIndex);
+  });
+
+  
+});
+
+
+$('.thumbnailPhoto').mouseover ((event2) => {
+  let photoHovered = $(event2.target).attr('data-number');
+  let number2Index = parseInt(photoHovered);
+  console.log(number2Index); 
+ $('.thumbnailTitles').show();
+
+$('.thumbnailPhoto').mouseleave ((event3) => {
+  let photoHovered2 = $(event3.target).attr('data-number');
+  let number3Index = parseInt(photoHovered2);
+  console.log(number3Index);
+  $('.thumbnailTitles').hide();
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
